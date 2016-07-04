58ae588c95e988a36ac300931f58553afc89f698
GABRIELFILMSTUIDOS <GFILMSTUDIOS@gmail.com>
2016-07-04 18:27:11 +0200

    Adding a shortcode class

---
+++ includes/lib/class-equipment-management-shortcode.php
@@ -0,0 +1,60 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Equipment_Management_Shortcode {
+
+	/**
+	 * The name for the shortcode.
+	 * @var 	string
+	 * @access  public
+	 * @since 	1.0.0
+	 */
+	public $shortcode;
+        
+        /**
+         * The handler function for this shortcode. It will be passed the attributes,
+         * with shortcode_atts already applied, as well as the content 'as is'.
+         * @var         callable
+         * @access  public
+         * @since       1.0.0
+         */
+        public $callback;
+        
+        /**
+         * The entire list of supported attributes and their defaults as in https://developer.wordpress.org/reference/functions/shortcode_atts/
+         * @var         array
+         * @access  public
+         * @since       1.0.0
+         */
+        public $pairs;
+
+	public function __construct ( $shortcode = '', $callback = '', $pairs = array() ) {
+
+		if ( ! $shortcode || ! $callback ) return;
+
+		// Post type name and labels
+		$this->shortcode = $shortcode;
+		$this->callback = $callback;
+		$this->pairs = $pairs;
+
+		// Regsiter post type
+		add_shortcode( $shortcode , array( $this, 'run_shortcode' ) );
+	}
+
+	/**
+	 * Run the shortcode
+         * 
+	 * @return void
+	 */
+	public function run_shortcode ( $atts, $content = '' ) {
+                
+		$atts = shortcode_atts( 
+                        $this->pairs,
+                        $this->atts,
+                        $this->shortcode);
+                
+                call_user_func($this->callback, $atts, $content);
+	}
+
+}
