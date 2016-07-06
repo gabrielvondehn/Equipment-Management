02d19715b0c7f30aa28921dd965c1d876cd4681a
GABRIELFILMSTUIDOS <GFILMSTUDIOS@gmail.com>
2016-07-06 15:24:19 +0200

    Bugfixes

--- includes/class-equipment-management-database.php
+++ includes/class-equipment-management-database.php
@@ -8,12 +8,12 @@ class Equipment_Management_Database extends Equipment_Management_Database_API {
     public function __construct() {
         global $wpdb;
         
-        $table_structure_json = file_get_contents(plugin_dir_url( __FILE__ ) .
-                "includes/equipment-management-database-table-structure.json");
+        $table_structure_json = file_get_contents(plugin_dir_url( __FILE__ ) . 
+                "/equipment-management-database-table-structure.json" );
         
         $table_structure = Equipment_Management_Database_API::parse_table_structure($table_structure_json);
         
-        super (
+        parent::__construct (
                 array(
                     "main_table" => ($wpdb->prefix)."equipment",
                     "use_table" => ($wpdb->prefix)."equipment_use",
