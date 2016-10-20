e7e9769d4b9fa6a2fe0e69c174ce186876e47bce
GABRIELFILMSTUIDOS <GFILMSTUDIOS@gmail.com>
2016-10-20 20:43:10 +0300

    Cleanup

--- includes/shortcodes/class-equipment-management-shortcodes.php
+++ includes/shortcodes/class-equipment-management-shortcodes.php
@@ -5,13 +5,8 @@ class Equipment_Management_Shortcodes {
     public $shortcodes = array();
    
     public function __construct() {
-        array_push($this->shortcodes, new Equipment_Management_Shortcode("equip_ls", array($this, "list_all")));
         array_push($this->shortcodes, new Equipment_Management_Shortcode(
                 "equip_show", array($this, "show_id"), array("id" => "")));
-    }
-    
-    public function list_all() {
-        
     }
     
     public function show_id( $attrs ) {
